baea648e3c5e40c94205bf7481d4630c4de7b52e
= <=>
2024-03-21 13:51:08 +0530

    updated setter getter codes in  Feature Branch

--- Test_Progs/OOPS_Concepts/getter_setter.py
+++ Test_Progs/OOPS_Concepts/getter_setter.py
@@ -1,5 +1,21 @@
+# # # Getter and Setter method---------------------------------------
+# class A:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#
+#     def set_meth(self):
+#         return self.name,self.age
+#
+#     def get_age(self):
+#         print(f'name is {self.name} and age is {self.age}')
+#
+# obj=A("Raj",22)
+# obj.set_meth()
+# obj.get_age()
+
 # # Getter and Setter method---------------------------------------
-class A:
+class B:
     def __init__(self,name,age):
         self.name=name
         self.age=age
@@ -8,8 +24,14 @@ class A:
         return self.name,self.age
 
     def get_age(self):
-        print(f'name is {self.name} and age is {self.age}')
+        if self.age>18:
+            print(f'name is {self.name} and age is {self.age}')
+        else:
+            print("Does not qualified")
 
-obj=A("Raj",22)
-obj.set_meth()
-obj.get_age()
+obj1=B("Raj",21)
+obj1.set_meth()
+obj1.get_age()
+obj2=B("Raj",15)
+obj2.set_meth()
+obj2.get_age()
